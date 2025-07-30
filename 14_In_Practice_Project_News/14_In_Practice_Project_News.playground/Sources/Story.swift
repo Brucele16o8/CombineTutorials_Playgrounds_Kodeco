86292e4a89b46f8eddd827615984c8db1cc5d9fc
Bruce Le <brucele.tt168@gmail.com>
2025-07-30 18:21:49 +1000

    News project tutorials completed

---
+++ 14_In_Practice_Project_News/14_In_Practice_Project_News.playground/Sources/Story.swift
@@ -0,0 +1,28 @@
+//
+//  Story.swift
+//  
+//
+//  Created by Tung Le on 30/7/2025.
+//
+
+import Foundation
+
+public struct Story: Codable {
+  public let id: Int
+  public let title: String
+  public let by: String
+  public let time: TimeInterval
+  public let url: String
+}
+
+extension Story: Comparable {
+  public static func < (lhs: Story, rhs: Story) -> Bool {
+    return lhs.time > rhs.time
+  }
+}
+
+extension Story: CustomDebugStringConvertible {
+  public var debugDescription: String {
+    return "\n\(title)\nby \(by)\nurl: \(url)\n------"
+  }
+}
